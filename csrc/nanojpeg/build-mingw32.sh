6720605ab89fd249791ee5d3e4dbd3ca0ea1c960
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:44:24 +0200

    changed binary paths in build scripts

--- csrc/nanojpeg/build-mingw32.sh
+++ csrc/nanojpeg/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc nanojpeg2.c -O3 -s -shared -o ../../bin/nanojpeg2.dll -DNJ_USE_LIBC \
+gcc nanojpeg2.c -O3 -s -shared -o ../../bin/mingw32/nanojpeg2.dll -DNJ_USE_LIBC \
 	-std=c99 -Wall -Wextra -pedantic -Werror
