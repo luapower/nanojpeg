09d829d0a75fcc6bbc619fcdfd836b9f6807039c
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:21 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/nanojpeg/build-linux32.sh
+++ csrc/nanojpeg/build-linux32.sh
@@ -1,2 +1,2 @@
-gcc nanojpeg2.c -O3 -s -shared -o ../../bin/linux32/libnanojpeg2.so -DNJ_USE_LIBC \
+gcc $CFLAGS nanojpeg2.c -shared -o ../../bin/linux32/libnanojpeg2.so -DNJ_USE_LIBC \
 	-std=c99 -Wall -Wextra -pedantic -Werror
--- csrc/nanojpeg/build-mingw32.sh
+++ csrc/nanojpeg/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc nanojpeg2.c -O3 -s -shared -o ../../bin/mingw32/nanojpeg2.dll -DNJ_USE_LIBC \
+gcc $CFLAGS nanojpeg2.c -shared -o ../../bin/mingw32/nanojpeg2.dll -DNJ_USE_LIBC \
 	-std=c99 -Wall -Wextra -pedantic -Werror
