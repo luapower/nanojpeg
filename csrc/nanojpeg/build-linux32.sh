efa00fd85464f28fafb3bca5b3fba6baae14ee22
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:17 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/nanojpeg/build-linux32.sh
+++ csrc/nanojpeg/build-linux32.sh
@@ -1,2 +1,2 @@
-gcc $CFLAGS nanojpeg2.c -shared -o ../../bin/linux32/libnanojpeg2.so -DNJ_USE_LIBC \
+gcc -O2 -s -static-libgcc nanojpeg2.c -shared -o ../../bin/linux32/libnanojpeg2.so -DNJ_USE_LIBC \
 	-std=c99 -Wall -Wextra -pedantic -Werror
--- csrc/nanojpeg/build-mingw32.sh
+++ csrc/nanojpeg/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc $CFLAGS nanojpeg2.c -shared -o ../../bin/mingw32/nanojpeg2.dll -DNJ_USE_LIBC \
+gcc -O2 -s -static-libgcc nanojpeg2.c -shared -o ../../bin/mingw32/nanojpeg2.dll -DNJ_USE_LIBC \
 	-std=c99 -Wall -Wextra -pedantic -Werror
