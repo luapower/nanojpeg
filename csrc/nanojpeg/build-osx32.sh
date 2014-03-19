b8cd600520603b324ebcfb587ac60bdaafaf28ed
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:11:23 +0200

    osx32 build

---
+++ csrc/nanojpeg/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 nanojpeg2.c -shared -o ../../bin/osx32/libnanojpeg2.dylib -DNJ_USE_LIBC -std=c99 -Wall -Wextra -pedantic -Werror
--- csrc/nanojpeg/build-osx64.sh
+++ csrc/nanojpeg/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 nanojpeg2.c -shared -o ../../bin/osx64/libnanojpeg2.dylib -DNJ_USE_LIBC -std=c99 -Wall -Wextra -pedantic -Werror
+gcc -arch x86_64 -O2 nanojpeg2.c -shared -o ../../bin/osx64/libnanojpeg2.dylib -DNJ_USE_LIBC -std=c99 -Wall -Wextra -pedantic -Werror
