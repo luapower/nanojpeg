1fcde4f18b50e8706e5e9dcedee25b06638ccea1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:08:45 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/nanojpeg/build-osx32.sh
+++ csrc/nanojpeg/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 nanojpeg2.c -shared -o ../../bin/osx32/libnanojpeg2.dylib -DNJ_USE_LIBC -std=c99 -Wall -Wextra -pedantic -Werror
+gcc -arch i386 -O2 nanojpeg2.c -shared -install_name @loader_path/libnanojpeg.dylib -o ../../bin/osx32/libnanojpeg2.dylib -DNJ_USE_LIBC -std=c99 -Wall -Wextra -pedantic -Werror
