b6e035b9c6cfc0e82c436032dba622c9f2e052f3
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:04:08 +0200

    unimportant

--- csrc/nanojpeg/build-osx32.sh
+++ csrc/nanojpeg/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libnanojpeg2.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libnanojpeg2.dylib" \
 	D=libnanojpeg2.dylib A=libnanojpeg2.a ./build.sh
