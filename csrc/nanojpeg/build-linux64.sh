f9c1bdf4ec3cced998da3d96a287f653e83a62c4
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 12:59:38 +0200

    unimportant

--- csrc/nanojpeg/build-linux64.sh
+++ csrc/nanojpeg/build-linux64.sh
@@ -1,1 +1,1 @@
-P=linux64 C=-fPIC L="-s -static-libgcc" D=libnanojpeg2.so A=libnanojpeg2.a ./build.sh
+P=linux64 C="-fPIC -include _memcpy.h" L="-s -static-libgcc" D=libnanojpeg2.so A=libnanojpeg2.a ./build.sh
